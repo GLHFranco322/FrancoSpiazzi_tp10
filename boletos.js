dd48678efbbd145cae32112f03f3dd8d2c48b427
GLHFranco322 <francospiazzi96@gmail.com>
2024-10-22 15:59:53 -0300

    Micro desafio 1 listo

---
+++ boletos.js
@@ -0,0 +1,20 @@
+let nombre = "Ana";
+let apellido = "González";
+let edad = 30;
+let numeroDeTelefono = "987-654-3210";
+let socio = false;
+let fechaDelPartido = "2024-12-01";
+let horaDelPartido = "20:30";
+let numeroDeSilla = 12;
+let precioDelBoleto = 200.50;
+
+console.log("Nombre:", nombre, " .El dato es tipo: ",typeof(nombre));
+console.log("Apellido:", apellido, " .El dato es tipo: ",typeof(apellido));
+console.log("Edad:", edad, " .El dato es tipo: ",typeof(edad));
+console.log("Número de Teléfono:", numeroDeTelefono, " .El dato es tipo: ",typeof(numeroDeTelefono));
+console.log("Socio:", socio, " .El dato es tipo: ",typeof(socio));
+console.log("Fecha del Partido:", fechaDelPartido, " .El dato es tipo: ",typeof(fechaDelPartido));
+console.log("Hora del Partido:", horaDelPartido, " .El dato es tipo: ",typeof(horaDelPartido));
+console.log("Número de Silla:", numeroDeSilla, " .El dato es tipo: ",typeof(numeroDeSilla));
+console.log("Precio del Boleto:", precioDelBoleto, " .El dato es tipo: ",typeof(precioDelBoleto));
+
